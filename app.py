fbc2290be5ebf338f6141487676792bfcedaa68e
Raul <raulmayas20@gmail.com>
2022-03-30 16:41:54 -0600

    Video Games Data Set

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 ## Dependencies
 import pymongo
+import json
+import pandas as pd
 from bson.json_util import dumps, loads
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS, cross_origin
+import plotly.express as px
+import plotly
 
 ## Flask Setup
 app = Flask(__name__)
@@ -18,7 +22,21 @@ db = client.VideoGamesDB
 ## Homepage
 @app.route("/")
 def home():
-    return render_template("/index.html")
+    query =  {'$or': [{'platform': 'PS4'},
+    {'platform': 'XOne'},
+    {'platform': 'Steam'}]}
+    
+    results = db.united.find(query)
+    preferred_platforms = pd.DataFrame(results)
+    preferred_platforms.head(10)
+
+    fig = px.histogram(preferred_platforms[:15], x="name", y="user_score",
+             color='platform', barmode='group',
+             height=400)
+    
+    graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("/index.html", df = graph)
 
 ## RUN FLASK APP
 if __name__ == "__main__":
